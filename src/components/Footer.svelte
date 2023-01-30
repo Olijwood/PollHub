3d05cc11768ccd4bde7381816f1f1a4b72263400
Olijwood <olijwood@gmail.com>
2023-01-30 17:25:11 +0000

    first commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<footer>
+    <div class="copyright">Copyright 2023 PollHUB</div>
+</footer>
+
+<style>
+    footer{
+        padding: 40px;
+    }
+    .copyright{
+        color: #aaa;
+        font-size: 14px;
+        display: inline-block;
+        padding: 20px;
+        border-top: 1px solid #ddd;
+    }
+</style>
